c16fb1c35bf1b4822bde23b5293fa39c8e7c0499
songforthemuto <huni0423@naver.com>
2022-05-16 20:30:11 +0900

    시저 암호

---
+++ programmers./caesarCipher.js
@@ -0,0 +1,16 @@
+// 시저 암호
+function solution(s, n) {
+    let answer = "";
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === " ") answer += " ";
+        else if (
+            (s[i].charCodeAt(0) <= 90 && s[i].charCodeAt(0) + n > 90) ||
+            (s[i].charCodeAt(0) >= 97 && s[i].charCodeAt(0) + n > 122)
+        )
+            answer += String.fromCharCode(s[i].charCodeAt(0) + n - 25 - 1);
+        else answer += String.fromCharCode(s[i].charCodeAt(0) + n);
+    }
+
+    return answer;
+}
