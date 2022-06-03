5cbf2063fe62dd92db756469c5bce2198e8ef4a5
songforthemuto <huni0423@naver.com>
2022-06-03 19:49:00 +0900

    오픈채팅방

---
+++ programmers./openChat.js
@@ -0,0 +1,22 @@
+// 오픈채팅방
+function solution(record) {
+    const arr = [];
+    const table = {};
+    for (const elem of record) arr.push(elem.split(" "));
+    for (const elem of arr) {
+        if (elem[0] !== "Leave") {
+            if (!table[elem[1]]) table[elem[1]] = elem[2];
+            else table[elem[1]] = elem[2];
+        }
+    }
+
+    const result = [];
+    for (const elem of arr) {
+        if (elem[0] === "Enter")
+            result.push(`${table[elem[1]]}님이 들어왔습니다.`);
+        else if (elem[0] === "Leave")
+            result.push(`${table[elem[1]]}님이 나갔습니다.`);
+    }
+
+    return result;
+}
