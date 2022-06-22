750a5525e8caa10a61da69f3f42fc870ec9ac628
songforthemuto <huni0423@naver.com>
2022-06-22 16:55:43 +0900

    duplicateEncoder

---
+++ codewars/duplicateEncoder.ts
@@ -0,0 +1,27 @@
+// 6kyu: Duplicate Encoder
+function duplicateEncode(word: string): string {
+    const splitted: string[] = word.toLowerCase().split("");
+
+    const result: string[] = [];
+    splitted.forEach((e: string) =>
+        result.push(word.toLowerCase().split(e).length - 1 <= 1 ? "(" : ")")
+    );
+
+    return result.join("");
+}
+
+function duplicateEncode2(word: string): string {
+    const box: { [key: string]: number } = {};
+    const word_lower: string = word.toLowerCase();
+    let result: string = "";
+
+    for (const e of word_lower) {
+        box[e] = !box[e] ? 1 : ++box[e];
+    }
+
+    for (const e of word_lower) {
+        result += box[e] === 1 ? "(" : ")";
+    }
+
+    return result;
+}
