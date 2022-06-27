e62eb5d7ce5ba7080bbe0b76586a9cbe0d8ca5eb
songforthemuto <huni0423@naver.com>
2022-06-28 00:09:19 +0900

    2. Add Two Numbers

---
+++ leetcode/0002.ts
@@ -0,0 +1,41 @@
+// 2. Add Two Numbers
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(
+    l1: ListNode | null,
+    l2: ListNode | null
+): ListNode | null {
+    let result: ListNode = new ListNode(0);
+    let current: ListNode = result;
+    let sum: number = 0;
+
+    while (l1 || l2 || sum) {
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        sum = sum >= 10 ? 1 : 0;
+    }
+
+    return result.next;
+}
