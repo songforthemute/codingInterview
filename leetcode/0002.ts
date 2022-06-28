f7a50067756c2cae079f3a9d90bdf4fbb7bfabfe
songforthemuto <huni0423@naver.com>
2022-06-28 20:01:56 +0900

    2. Add Two Numbers

--- leetcode/0002.ts
+++ leetcode/0002.ts
@@ -12,6 +12,15 @@
  * }
  */
 
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
 function addTwoNumbers(
     l1: ListNode | null,
     l2: ListNode | null
