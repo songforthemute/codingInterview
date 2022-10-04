01856c1e8903954fbb061b6cccabbfdecaf2c2a6
songforthemuto <huni0423@naver.com>
2022-10-04 19:36:27 +0900

    크레인 인형뽑기 게임
    Lv. 1 (Refactoring)

--- programmers./crainGame.js
+++ programmers./crainGame.js
@@ -1,37 +1,35 @@
 // 크레인 인형뽑기 게임
-function solution(board, moves) {
-    let match = 0; // 같아서 터진 횟수
-    const catched = []; // 뽑은 인형의 바구니
-    const box = [];
-    let temp = [];
-    let row = 0,
-        col = 0;
 
-    while (row !== board.length) {
-        if (board[col][row] !== 0) temp.push(board[col][row]);
-        col++;
-        if (col === board.length) {
-            col = 0;
-            box.push(temp);
-            temp = [];
-            row++;
+/**
+ * @param {number[][]} board
+ * @param {number[]} moves
+ * @return {number}
+ */
+
+const solution = (board, moves) => {
+    let count = 0;
+    const stack = [];
+
+    for (const turn of moves) {
+        let now = turn - 1;
+        let x = 0;
+
+        while (x !== board.length) {
+            if (board[x][now] !== 0) {
+                if (stack.length && stack[stack.length - 1] === board[x][now]) {
+                    board[x][now] = 0;
+                    stack.pop();
+                    count += 2;
+                } else {
+                    stack.push(board[x][now]);
+                    board[x][now] = 0;
+                }
+                break;
+            }
+
+            x++;
         }
     }
 
-    for (let i = 0; i < moves.length; i++) {
-        if (box[moves[i] - 1].length > 0) {
-            catched.push(box[moves[i] - 1][0]);
-            box[moves[i] - 1].shift();
-        } else {
-            continue;
-        }
-
-        if (catched[catched.length - 1] === catched[catched.length - 2]) {
-            catched.pop();
-            catched.pop();
-            match += 2;
-        }
-    }
-
-    return match;
-}
+    return count;
+};
