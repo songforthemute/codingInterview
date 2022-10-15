de46e2d3b61ab874baba0301f08a061ce8fac0ca
songforthemuto <huni0423@naver.com>
2022-10-15 19:39:15 +0900

    삼총사
    Lv. 1

--- programmers./trio.js
+++ programmers./trio.js
@@ -27,3 +27,19 @@ function solution(number) {
 
     return count;
 }
+
+function solution2(number) {
+    let count = 0;
+
+    for (let i = 0; i < number.length - 2; i++) {
+        for (let j = i + 1; j < number.length - 1; j++) {
+            for (let k = j + 1; k < number.length; k++) {
+                if (number[i] + number[j] + number[k] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
