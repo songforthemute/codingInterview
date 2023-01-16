cd61e5495005b469be5b527bd45f5c437d2d3c67
0rganicpotato <121741455+0rganicPotato@users.noreply.github.com>
2023-01-16 18:10:58 +0900

    lv 01 두 수의 차

--- java/programmers/level_0/src/두_수의_차.java
+++ java/programmers/level_0/src/두_수의_차.java
@@ -1,12 +1,12 @@
-public class 두_수의_곱 {
 
+
+public class 두_수의_차 {
 
   private static int solution(int num1, int num2) {
-    return num1 * num2;
+    return num1 - num2;
   }
 
   public static void main(String[] args) {
-    System.out.println(solution(10, 20));
-
+    System.out.println(solution(5, 3));
   }
 }
