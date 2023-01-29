6d5b4baa0e55178d6f7f8b2c6221e047c88ba67e
springPotatoes <121741455+0rganicPotato@users.noreply.github.com>
2023-01-30 08:59:35 +0900

    Solve: lv0 인덱스 바꾸기 (java)

--- java/programmers/level_0/src/인덱스_바꾸기.java
+++ java/programmers/level_0/src/인덱스_바꾸기.java
@@ -1,6 +1,7 @@
 public class 인덱스_바꾸기 {
 
   //문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때,
+  //my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
   public static String solution(String my_string, int num1, int num2) {
     String answer = "";
 
@@ -16,7 +17,7 @@ public class 인덱스_바꾸기 {
 
     return answer;
   }
-//my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+
 
   public static void main(String[] args) {
     System.out.println(solution("I love you", 3, 6));
