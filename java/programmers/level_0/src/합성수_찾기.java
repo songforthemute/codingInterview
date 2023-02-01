309d40ca35bfdcde37f70762344e0f24b0302002
springPotatoes <121741455+springPotatoes@users.noreply.github.com>
2023-02-01 23:19:54 +0900

    Solve: lv0 합성수 찾기 (java)

--- java/programmers/level_0/src/합성수_찾기.java
+++ java/programmers/level_0/src/합성수_찾기.java
@@ -4,7 +4,6 @@ public class 합성수_찾기 {
 // 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
     public int solution(int n) {
         int answer = 0;
-
         for (int i = 1; i <= n; i++) {
             int tmp = 0;
 
